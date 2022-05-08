835729a644f1d01c5ad7b0629d1bdd1758524efb
jeffgbanziali <91453731+jeffgbanziali@users.noreply.github.com>
2022-05-08 13:49:39 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import SignInPage from './components/SignInPage/SignInPage'
+
+function App () {
+  return (
+    <main className='App'>
+      <SignInPage />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/SignInPage/dist/SignInPage.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _react = require("react");
+
+var _freeSolidSvgIcons = require("@fortawesome/free-solid-svg-icons");
+
+var _reactFontawesome = require("@fortawesome/react-fontawesome");
